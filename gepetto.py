3aedbcb5b258a3cbf6dc0dad95a704d7c23b1653
Thomas Butler <trbiv@proton.me>
2025-05-15 19:29:39 -0500

    add bac unque bas

--- gepetto.py
+++ gepetto.py
@@ -398,8 +398,8 @@ class Gepetto:
         logger.info(f"Undeployed chutes: {undeployed_chutes}")
         logger.info(f"All viable chutes: {chute_values}")
         chutes_to_scale = chute_values
-        #if undeployed_chutes:
-            #chutes_to_scale = undeployed_chutes
+        if undeployed_chutes:
+            chutes_to_scale = undeployed_chutes
 
         chutes_to_scale.sort(key=lambda x: x[2], reverse=True)
 
